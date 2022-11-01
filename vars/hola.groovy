fda00dacf972c18253aaf576b17c939ab9fdff0b
Flezxfp <116027556+Flezxfp@users.noreply.github.com>
2022-11-01 16:57:55 -0600

    Update hola.groovy
    se cambio el pipeline

--- vars/hola.groovy
+++ vars/hola.groovy
@@ -9,9 +9,9 @@ def call() {
     }
 
   stages {
-      stage('Deigo') {
+      stage('Dieguini ') {
       steps {
-           bat '''sh  vars/hola.groovy '''
+           bat """ type pipes/vars/saludo.groovy """
         }
 
   post {
