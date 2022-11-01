a8e8a87e48fbf0473db862e404bfefb8e7ef839e
Flezxfp <116027556+Flezxfp@users.noreply.github.com>
2022-11-01 17:04:03 -0600

    se modificaron los groovy

--- vars/angularNode14.groovy
+++ vars/angularNode14.groovy
@@ -9,9 +9,9 @@ def call() {
     }
 
   stages {
-      stage('Deigo') {
+      stage('Dieguini ') {
       steps {
-           bat ''' cat pipes/vars/saludo.groovy '''
+           bat """ type pipes/vars/saludo.groovy """
         }
 
   post {
