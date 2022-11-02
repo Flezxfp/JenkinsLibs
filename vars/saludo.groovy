0a7c80cb6781ce115078f748dedaa22ab38c2079
Flezxfp <116027556+Flezxfp@users.noreply.github.com>
2022-11-01 18:48:03 -0600

    asludos

--- vars/saludo.groovy
+++ vars/saludo.groovy
@@ -11,14 +11,9 @@ def call() {
   stages {
       stage('Dieguini ') {
       steps {
-<<<<<<< HEAD
-           bat """ type pipes/vars/saludo.groovy """
-=======
-
            bat """ git clone https://github.com/Flezxfp/JenkinsLibs.git"""  
           //  bat """ type pipes/vars/saludo.groovy """
-          bat """pwd"""
->>>>>>> f6a11df58d54c9d27fbc3032824c2e87a2c7d230
+          bat """cd"""
         }
 
   post {
